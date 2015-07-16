487496b7cb250621226042ba9b2c84be4588953c
Chris Borckholder <borckholder@gmail.com>
2015-07-16 14:52:34 +0200

    Update .editorconfig
    Sets indentation for *.py files to 4. Reformat existing code.

--- 1.py
+++ 1.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 import sys
 
+
 def solve(n):
-  return arithmetic_series(3, n) + arithmetic_series(5, n) - arithmetic_series(15, n)
+    return arithmetic_series(3, n) + arithmetic_series(5, n) - arithmetic_series(15, n)
+
 
 def arithmetic_series(d, n):
-  top = n - (n % d)
-  return (d + top) * (n // d) // 2
+    top = n - (n % d)
+    return (d + top) * (n // d) // 2
+
 
 cases = sys.stdin.readlines()[1:]
-#cases = ["10", "100"]
 for line in cases:
-  print(solve(int(line) - 1))
+    print(solve(int(line) - 1))
--- 2.py
+++ 2.py
@@ -2,19 +2,21 @@
 import sys
 from itertools import islice
 
+
 def solve(n):
-  return sum(islice(fib(n), 2, None, 3))
+    return sum(islice(fib(n), 2, None, 3))
+
 
 def fib(limit):
-  predecessor = 0
-  current = 1
-  while current < limit:
-    yield current
-    temp = current
-    current = current + predecessor
-    predecessor = temp
+    predecessor = 0
+    current = 1
+    while current < limit:
+        yield current
+        temp = current
+        current = current + predecessor
+        predecessor = temp
+
 
 cases = sys.stdin.readlines()[1:]
-#cases = ["0", "10", "100"]
 for line in cases:
-  print(solve(int(line)))
+    print(solve(int(line)))
